40b96c3492a4293e87fd7cc8165d3ecd979caa96
akmalhasan19 <akmal.ryan1@gmail.com>
2026-01-29 19:21:37 +0700

    Image Optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'haknornfainyyfrnzyxp.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+
   async headers() {
     return [
       {

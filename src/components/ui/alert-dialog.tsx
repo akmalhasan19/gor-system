3f082bda19218b85c243661c2ec78044c1d21447
akmalhasan19 <akmal.ryan1@gmail.com>
2026-02-04 10:58:35 +0700

    Masalah Tombol Hapus Fixed

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -28,11 +28,11 @@ export const AlertDialog: React.FC<AlertDialogProps> = ({
         <AlertDialogPrimitive.Root open={isOpen} onOpenChange={(open) => !open && onClose()}>
             <AlertDialogPrimitive.Portal>
                 {/* Overlay */}
-                <AlertDialogPrimitive.Overlay className="fixed inset-0 bg-black/50 z-[60] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+                <AlertDialogPrimitive.Overlay className="fixed inset-0 bg-black/50 z-[10000] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
 
                 {/* Content */}
                 <AlertDialogPrimitive.Content
-                    className="fixed left-[50%] top-[50%] z-[60] translate-x-[-50%] translate-y-[-50%] bg-white border-2 border-black shadow-neo w-full max-w-sm flex flex-col data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] duration-200"
+                    className="fixed left-[50%] top-[50%] z-[10000] translate-x-[-50%] translate-y-[-50%] bg-white border-2 border-black shadow-neo w-full max-w-sm flex flex-col data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] duration-200"
                 >
                     {/* Header */}
                     <div className="bg-black text-white p-3 flex justify-between items-center border-b-2 border-black">
